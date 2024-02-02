daf1278e54ba09658b6b99a237eff80fb3eca09d
NavyStack <137406386+Navystack@users.noreply.github.com>
2024-02-02 08:46:44 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import starlight from '@astrojs/starlight'
-import starlightLinksValidator from 'starlight-links-validator'
-import { rehypeHeadingIds } from '@astrojs/markdown-remark'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import compress from 'astro-compress'
-import preact from '@astrojs/preact'
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import compress from 'astro-compress';
+import preact from '@astrojs/preact';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,63 +13,47 @@ export default defineConfig({
     prefetchAll: true
   },
   site: 'https://syno.ossdocs.org',
-  integrations: [
-    starlight({
-      plugins: [starlightLinksValidator()],
-      title: 'Syno Docs',
-      defaultLocale: 'root',
-      locales: {
-        root: {
-          label: '한국어',
-          lang: 'ko'
-        },
-        en: {
-          label: 'English'
-        },
-        jp: {
-          label: '日本語'
-        },
-        'zh-cn': {
-          label: '简体中文',
-          lang: 'zh-CN'
-        }
+  integrations: [starlight({
+    plugins: [starlightLinksValidator()],
+    title: 'Syno Docs',
+    defaultLocale: 'root',
+    locales: {
+      root: {
+        label: '한국어',
+        lang: 'ko'
       },
-      components: {
-        Head: './src/components/Head.astro'
+      en: {
+        label: 'English'
       },
-      customCss: [
-        './src/styles/custom.css',
-        './node_modules/pretendard/dist/web/static/pretendard.css',
-        './src/styles/headings.css',
-        './src/styles/linking.css'
-      ],
-      social: {
-        github: 'https://github.com/OssWithDocs/Syno/'
+      jp: {
+        label: '日本語'
       },
-      editLink: {
-        baseUrl: 'https://github.com/OssWithDocs/Syno/edit/main/'
+      'zh-cn': {
+        label: '简体中文',
+        lang: 'zh-CN'
       }
-    }),
-    compress({
-      CSS: true,
-      HTML: true,
-      Image: false,
-      JavaScript: true,
-      SVG: true
-    }),
-    preact()
-  ],
+    },
+    components: {
+      Head: './src/components/Head.astro'
+    },
+    customCss: ['./src/styles/custom.css', './node_modules/pretendard/dist/web/static/pretendard.css', './src/styles/headings.css', './src/styles/linking.css'],
+    social: {
+      github: 'https://github.com/OssWithDocs/Syno/'
+    },
+    editLink: {
+      baseUrl: 'https://github.com/OssWithDocs/Syno/edit/main/'
+    }
+  }), compress({
+    CSS: true,
+    HTML: true,
+    Image: false,
+    JavaScript: true,
+    SVG: true
+  }), preact()],
   markdown: {
-    rehypePlugins: [
-      rehypeHeadingIds,
-      [
-        rehypeAutolinkHeadings,
-        {
-          // Wrap the heading text in a link.
-          behavior: 'wrap'
-        }
-      ],
-      rehypeAutolinkHeadings
-    ]
+    rehypePlugins: [rehypeHeadingIds, [rehypeAutolinkHeadings, {
+      // Wrap the heading text in a link.
+      behavior: 'wrap'
+    }], rehypeAutolinkHeadings]
   }
-})
+});
