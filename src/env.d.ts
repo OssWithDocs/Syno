53b2cee6607e9da391d4e759c5038f766c40031a
NavyStack <137406386+Navystack@users.noreply.github.com>
2024-02-02 09:54:02 +0000

    .

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,23 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 // type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+
+interface RemarkConfig {
+  host: string
+  site_id?: string
+  url?: string
+  components?: Array<'embed' | 'last-comments' | 'counter'>
+  max_shown_comments?: number
+  max_last_comments?: number
+  theme?: 'light' | 'dark'
+  page_title?: string
+  locale?: string
+  show_email_subscription?: boolean
+  show_rss_subscription?: boolean
+  simple_view?: boolean
+  no_footer?: boolean
+}
+
+interface Window {
+  remark_config: RemarkConfig
+}
